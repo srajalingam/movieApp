9f3877921baf3ede1df05eaebc38a3d75c674092
srajalingam <rajalingam299@gmail.com>
2026-07-14 23:56:39 +0530

    Set application layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,35 @@
+import Home from "./components/Home";
 
 
 function App() {
   return (
-    <div>
-      Go watch a Moview
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go watch a movie</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!"><span className="badge bg-success">Login</span></a>
+        </div>
+        <hr className="mb-3"/>
+      </div>
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">Home</a>
+              <a href="#!" className="list-group-item list-group-item-action">Movies</a>
+              <a href="#!" className="list-group-item list-group-item-action">Genres</a>
+              <a href="#!" className="list-group-item list-group-item-action">Add Movie</a>
+              <a href="#!" className="list-group-item list-group-item-action">Manage Catalogue</a>
+              <a href="#!" className="list-group-item list-group-item-action">GraphQl</a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+          <Home/>
+        </div>
+      </div>
     </div>
   );
 }
