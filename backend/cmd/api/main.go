034c6e431b6b434c07ea7e161ed190600a56ab03
srajalingam <rajalingam299@gmail.com>
2026-07-15 23:57:54 +0530

    Backend go build in http server created

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	//set application config
+	var app application
+
+	//read the command line
+
+	//connect to the database
+
+	app.Domain = "example.com"
+
+	log.Println("application running", port)
+
+	//start a web server
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
