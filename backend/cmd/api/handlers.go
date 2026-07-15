7e4c8dbffb441391e783be049a39e0ba927293f7
srajalingam <rajalingam299@gmail.com>
2026-07-16 00:40:51 +0530

    List of movies handler created

--- backend/cmd/api/handlers.go
+++ backend/cmd/api/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type home struct {
@@ -20,6 +22,45 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "1986-03-07")
+
+	highlander := models.Movie{
+		Id:          1,
+		Title:       "Highlander",
+		ReleaseDate: rd,
+		MPAARating:  "R",
+		RunTime:     116,
+		Description: "A very nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	movies = append(movies, highlander)
+
+	rd, _ = time.Parse("2006-01-02", "1981-06-24")
+	rotla := models.Movie{
+		Id:          1,
+		Title:       "Raiders of the Lost Ark",
+		ReleaseDate: rd,
+		MPAARating:  "PG-13",
+		RunTime:     115,
+		Description: "Another very nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	movies = append(movies, rotla)
+
+	out, err := json.Marshal(movies)
 	if err != nil {
 		fmt.Println(err)
 	}
