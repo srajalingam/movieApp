eb732a393c9fe1be8a389faebdac7f8f941c0959
srajalingam <rajalingam299@gmail.com>
2026-07-16 00:26:15 +0530

    Return json from API

--- backend/cmd/api/handlers.go
+++ backend/cmd/api/handlers.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world")
+type home struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Version string `json:"version"`
+}
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	var payload = &home{
+		Status:  "active",
+		Message: "Go movies up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
