9c7caaa2b2763fb9d9108b132948e9e1898f9e96
srajalingam <rajalingam299@gmail.com>
2026-07-16 00:12:55 +0530

    Configure Chi 3rd party rouer package to handle

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -23,11 +23,11 @@ func main() {
 	app.Domain = "example.com"
 
 	log.Println("application running", port)
-	http.HandleFunc("/", Hello)
+	// http.HandleFunc("/", Hello)
 
 	//start a web server
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 
 	if err != nil {
 		log.Fatal(err)
---
+++ readme.md
@@ -0,0 +1,5 @@
+Backend
+
+    go run .\cmd\api\
+
+    go get -u github.com/go-chi/chi/v5
