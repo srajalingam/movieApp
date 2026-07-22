de233ee48128f1ffebd36da4b76aab7309b681ec
srajalingam <rajalingam299@gmail.com>
2026-07-22 23:32:21 +0530

    write json reusable created

--- backend/cmd/api/handlers.go
+++ backend/cmd/api/handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -22,13 +21,7 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 		Version: "1.0.0",
 	}
 
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
@@ -39,12 +32,6 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 		return
 	}
+	_ = app.writeJSON(w, http.StatusOK, movies)
 
-	out, err := json.Marshal(movies)
-	if err != nil {
-		fmt.Println(err)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
 }
