56f7bb921a729110248d6204a27c3b1ee2dd7e50
srajalingam <rajalingam299@gmail.com>
2026-08-05 00:13:45 +0530

    get movie and geners from DB

--- backend/internal/repository/dbrepo/postgres_dbrepo.go
+++ backend/internal/repository/dbrepo/postgres_dbrepo.go
@@ -114,3 +114,169 @@ func (m *PostgresDBRepo) GetUserById(id int) (*models.User, error) {
 	return &user, nil
 
 }
+
+func (m *PostgresDBRepo) OneMovie(id int) (*models.Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+		select 
+			id,title,release_date,runtime,mpaa_rating,description,coalesce(image,''),created_at,updated_at
+		from
+			movies
+		where
+			id = $1
+	`
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	var movie models.Movie
+	err := row.Scan(
+		&movie.Id,
+		&movie.Title,
+		&movie.ReleaseDate,
+		&movie.RunTime,
+		&movie.MPAARating,
+		&movie.Description,
+		&movie.Image,
+		&movie.CreatedAt,
+		&movie.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the genres for the movie
+	query = `
+		select 
+			g.id, g.genre
+		from 
+			movie_genres mg
+		left join 
+			genres g on (g.id = mg.genre_id)
+		where 
+			mg.movie_id = $1
+			order by g.genre
+	`
+	rows, err := m.DB.QueryContext(ctx, query, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var genres []models.Genre
+	for rows.Next() {
+		var genre models.Genre
+		err := rows.Scan(
+			&genre.ID,
+			&genre.Genre,
+		)
+		if err != nil {
+			return nil, err
+		}
+		genres = append(genres, genre)
+	}
+
+	movie.Generes = genres
+
+	return &movie, nil
+
+}
+
+func (m *PostgresDBRepo) OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `
+		select 
+			id,title,release_date,runtime,mpaa_rating,description,coalesce(image,''),created_at,updated_at
+		from
+			movies
+		where
+			id = $1
+	`
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	var movie models.Movie
+	err := row.Scan(
+		&movie.Id,
+		&movie.Title,
+		&movie.ReleaseDate,
+		&movie.RunTime,
+		&movie.MPAARating,
+		&movie.Description,
+		&movie.Image,
+		&movie.CreatedAt,
+		&movie.UpdatedAt,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Get the genres for the movie
+	query = `
+		select 
+			g.id, g.genre
+		from 
+			movie_genres mg
+		left join 
+			genres g on (g.id = mg.genre_id)
+		where 
+			mg.movie_id = $1
+			order by g.genre
+	`
+	rows, err := m.DB.QueryContext(ctx, query, id)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer rows.Close()
+
+	var genres []models.Genre
+	var genesArray []int
+
+	for rows.Next() {
+		var genre models.Genre
+		err := rows.Scan(
+			&genre.ID,
+			&genre.Genre,
+		)
+		if err != nil {
+			return nil, nil, err
+		}
+		genres = append(genres, genre)
+		genesArray = append(genesArray, genre.ID)
+	}
+
+	movie.Generes = genres
+	movie.GenresArray = genesArray
+
+	var allGenres []*models.Genre
+
+	query = `
+		select 
+			id, genre
+		from
+			genres
+		order by
+			genre
+	`
+	rows, err = m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var genre models.Genre
+		err := rows.Scan(
+			&genre.ID,
+			&genre.Genre,
+		)
+		if err != nil {
+			return nil, nil, err
+		}
+		allGenres = append(allGenres, &genre)
+	}
+
+	return &movie, allGenres, nil
+
+}
