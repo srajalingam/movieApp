bcda344d4e6bab747e62cf8342cb2244ee2699bf
srajalingam <rajalingam299@gmail.com>
2026-08-05 00:53:43 +0530

    Movie api integrated in frontend

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -5,21 +5,39 @@ const Movie=()=>{
     const [movie,setMovie]=useState({})
     let {id}=useParams();
     useEffect(()=>{
-        let myMovie={
-            id:1,
-            title:"Highlander",
-            release_data:"1986-03-07",
-            runtime:116,
-            mpaa_rating:"R",
-            description:"Some long description"
+       const headers=new Headers();
+         headers.append("Content-Type","application/json")
+         const requestOptions={
+                method:"GET",
+                headers:headers,
         }
-        setMovie(myMovie)
+        fetch(`http://localhost:8080/movies/${id}`,requestOptions)
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data)
+            setMovie(data)
+        })
     },[id])
+    if (movie.genres && movie.genres.length > 0) {
+        movie.genres = Object.values(movie.genres);
+    }else {
+        movie.genres = [];
+    }
     return(
        <div>
             <h2>Movie:{movie.title}</h2>
-            <small><em>{movie.release_data},{movie.runtime} minutes, rated {movie.mpaa_rating}</em></small>
+            <small><em>{movie.release_date},{movie.runtime} minutes, rated {movie.mpaa_rating}</em></small> <br/>
+            {
+                movie.genres.map((g,index)=>(
+                    <span key={g.genre} className="badge bg-secondary me-2">
+                        {g.genre}
+                    </span>
+                ))
+            }
             <hr/>
+            {movie.image && (
+                <img src={`https://image.tmdb.org/t/p/w200${movie.image}`} alt={movie.title} className="img-fluid" />
+            )}  
             <p>{movie.description}</p>
         </div>
     )
